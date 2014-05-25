b3362dd8c0d1d2288be537ecb49f47e8888f392b
Rex Kerr <ichoran@gmail.com>
2014-05-25 14:18:08 -0700

    Version used for scanning.  More fixes and filters.  Still not very user-friendly.

--- Scasm.scala
+++ Scasm.scala
@@ -71,19 +71,27 @@ object Scasm {
     }
   }
   
-  class FieldPeeker(p: String => Boolean) extends asm.ClassVisitor(asm.Opcodes.ASM5) with Resulting[Option[String]] {
+  class FieldPeeker(p: String => Boolean, scalaOnly: Boolean = false) extends asm.ClassVisitor(asm.Opcodes.ASM5) with Resulting[Option[(String,String)]] {
     private[this] var myName: Option[String] = None
+    private[this] var staticValue: Option[Object] = None
     private[this] var hasNamedField: Boolean = false
+    private[this] var isScala: Boolean = !scalaOnly
     override def visit(version: Int, access: Int, name: String, sigs: String, sup: String, face: Array[String]) {
       myName = Some(name)
     }
     override def visitField(access: Int, name: String, desc: String, sig: String, value: Object) = {
-      if (p(name)) hasNamedField = true
+      if (p(name)) {
+        hasNamedField = true
+        staticValue = Option(value)
+      }
       null
     }
-    def clear() { myName = None; hasNamedField = false }
-    def result() = {
-      val ans = myName.filter(_ => hasNamedField)
+    override def visitAttribute(att: asm.Attribute) {
+      att.`type` match { case "Scala" | "ScalaSig" => isScala = true; case _ => }
+    }
+    def clear() { myName = None; staticValue = None; hasNamedField = false }
+    def result(): Option[(String, String)] = {
+      val ans = for (n <- myName if hasNamedField && isScala; v <- staticValue) yield (n, v.toString)
       clear()
       ans
     }
@@ -163,10 +171,10 @@ object Scasm {
   def methods(bytes: Array[Byte]) = visit(bytes, new ExtractMethods)
   def methods(f: File) = visit(f, new ExtractMethods)
   
-  def findField(name: String, bytes: Array[Byte], pr: String => Unit) {
-    val fp = new FieldPeeker(_ == name)
+  def findField(name: String, bytes: Array[Byte], pr: String => Unit, scalaOnly: Boolean = false) {
+    val fp = new FieldPeeker(_ == name, scalaOnly)
     visit(bytes, fp) match {
-      case Some(who) => pr(who)
+      case Some((who, what)) => pr(who + (if (what.length > 0) " = " + what + "." else ""))
       case _ =>
     }
   }
@@ -269,21 +277,32 @@ object Scasm {
     }.toMap
     val args = argsAndOps.takeWhile(_ != "--").filter(! _.startsWith("--")) ++ argsAndOps.dropWhile(_ != "--").drop(1)
     
+    def isNo(s: String) = s.toLowerCase match { case "n" | "no" | "false" => true; case _ => false }
+    
     val output = ops.get("output")
+    val allow: String => Boolean = ops.get("noAnonFunctions").filterNot(isNo) match {
+      case None => _ => true
+      case _ => s => !((s contains "$$anonfun") && s.endsWith(" = 0."))
+    }
+    
     def writeWith[A](f: (String => Unit) => A): Try[A] = output match {
-      case None => Success(f((s: String) => println(s)))
+      case None => Success(f((s: String) => if (allow(s)) println(s)))
       case Some(s) => Try {
         val fos = new FileOutputStream(s)
         try {
           val pw = new PrintWriter(fos)
-          try { f((s: String) => pw.println(s)) } finally { pw.close }
+          try { f((s: String) => if (allow(s)) pw.println(s)) } finally { pw.close }
         }
         finally (fos.close)
       }
     }
     
+    val scalaOnly = ops.get("scalaOnly").map(_.toLowerCase match { case "n" | "no" | "false" => false; case _ => true }).getOrElse(false)
+    
     ops.get("hasField").foreach{ field =>
-      if (field.length > 0) writeWith(wr => classBytes(args).foreach(bs => findField(field, bs, wr)))
+      if (field.length > 0) writeWith(wr => classBytes(args).foreach(bs => findField(field, bs, wr, scalaOnly)))
     }
   }
 }
+
+
